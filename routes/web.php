fca8be8024f1b29d6d8c9a778dae0e288b01cb79
Hafidh Asyi <hafidhasyisalahuddin@gmail.com>
2025-02-23 20:23:51 +0700

    add pages and routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,38 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homePage');
 });
+
+Route::get('/category', function () {
+    return view('categoryPage');
+});
+
+Route::get('/year', function () {
+    return view('yearPage');
+});
+
+Route::get('/detail', function () {
+    return view('detailPage');
+});
+
+Route::get('/alarmHistory', function () {
+    return view('alarmHistoryPage');
+});
+
+Route::get('/reportHistory', function () {
+    return view('reportHistoryPage');
+});
+
+Route::get('/edit', function () {
+    return view('editPage');
+});
+
+Route::get('/add', function () {
+    return view('addPage');
+});
+
+Route::get('/welcome', function () {
+    return view('wellcome');
+});
+
